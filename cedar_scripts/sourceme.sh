aae211131c5e937e1ddef1dc3f71f59c45bdc90a
Nick Prouse <nprouse@triumf.ca>
2019-07-01 19:02:58 -0700

    Add script to source at start

---
+++ cedar_scripts/sourceme.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+module load gcc/4.9.4
+module load python/3.6.3
+module load scipy-stack
+source /project/rpp-tanaka-ab/machine_learning/production_software/root/install/bin/thisroot.sh
+source /project/rpp-tanaka-ab/machine_learning/production_software/Geant4/geant4.10.01.p03-install/bin/geant4.sh
+source /project/rpp-tanaka-ab/machine_learning/production_software/Geant4/geant4.10.01.p03-install/share/Geant4-10.1.3/geant4make/geant4make.sh
+export G4WORKDIR=/project/rpp-tanaka-ab/machine_learning/production_software/WCSim/exe
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}${G4LIB}/${G4SYSTEM}
+export WCSIMDIR=/project/rpp-tanaka-ab/machine_learning/production_software/WCSim
+export PYTHONPATH=$ROOTSYS/../bindings/pyroot:$PYTHONPATH
