7becb81d37ee6645572fc63d4e16c232a08fdce4
Nick Prouse <nprouse@triumf.ca>
2019-07-04 21:13:32 -0700

    Add data quality jupyter notebook, code for making a few plots

--- data_quality/wcsim_data_quality.py
+++ data_quality/wcsim_data_quality.py
@@ -1,6 +1,8 @@
 import argparse
 from root_utils.root_file_utils import *
 
+ROOT.gROOT.SetBatch(True)
+
 
 def get_args():
     parser = argparse.ArgumentParser(description='dump WCSim data into numpy .npz file')
@@ -12,6 +14,45 @@ def get_args():
 def process_fileset(name, files):
     wcsim = WCSimChain(files)
     print(wcsim.nevent, "events in file set", name)
+    digiHitTime = ROOT.TH1D("digiHitTime", name+";Digitized hit times [ns]", 1350, 550, 1900)
+    digiHitCharge = ROOT.TH1D("digiHitCharge", name+";Digitized hit charge", 1000, 0.1, 100)
+    totalCharge = ROOT.TH1D("totalCharge", name+";Total charge", 1000, 100, 100000)
+    digiHitTime.SetStats(0)
+    digiHitCharge.SetStats(0)
+    totalCharge.SetStats(0)
+    digiHitTime.GetXaxis().SetTitleSize(0.05)
+    digiHitCharge.GetXaxis().SetTitleSize(0.05)
+    totalCharge.GetXaxis().SetTitleSize(0.05)
+    for ev in range(wcsim.nevent):
+        if ev % 100 == 0: print("event", ev, "of", wcsim.nevent, flush=True)
+        wcsim.get_event(ev)
+        trigger = wcsim.get_first_trigger()
+        ncherenkovdigihits = trigger.GetNcherenkovdigihits()
+        totalQ = 0
+        for i in range(ncherenkovdigihits):
+            hit = trigger.GetCherenkovDigiHits().At(i)
+            digiHitTime.Fill(hit.GetT())
+            Q = hit.GetQ()
+            totalQ += Q
+            digiHitCharge.Fill(Q)
+        totalCharge.Fill(totalQ)
+    if not os.path.isdir(name):
+        os.mkdir(name)
+    path=os.path.abspath(name)
+    c = ROOT.TCanvas()
+    digiHitTime.Draw()
+    c.SetLogy()
+    c.SaveAs(path+"/digiHitTime.pdf")
+    digiHitCharge.Draw()
+    c.SetLogy()
+    c.SetLogx()
+    c.Draw()
+    c.SaveAs(path+"/digiHitCharge.pdf")
+    totalCharge.Draw()
+    c.SetLogy()
+    c.SetLogx()
+    c.Draw()
+    c.SaveAs(path+"/totalCharge.pdf")
 
 
 if __name__ == '__main__':
