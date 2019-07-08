f8a99cd2086b87f0c92d668be198f8ec31be36e8
Nick Prouse <n.w.prouse@qmul.ac.uk>
2019-07-08 13:04:05 -0700

    update example job submission script

--- cedar_scripts/submit_jobs.sh
+++ cedar_scripts/submit_jobs.sh
@@ -4,19 +4,17 @@
 # exit when any command fails
 set -e
 
+# initial environment setup
 export DATATOOLS=/project/rpp-tanaka-ab/machine_learning/production_software/DataTools
 cd $DATATOOLS/cedar_scripts
-
 source sourceme.sh
 
+# name and output data directory for this run
 name=IWCDmPMT_test
 data_dir=/project/rpp-tanaka-ab/machine_learning/data
 
+# Run setup scripts
 source setup_jobs.sh "$name" "$data_dir"
-
-# set WCSim directory to where the data will also go (setup script will compile it there to be reused if needed)
-export WCSIMDIR="$data_dir/$name/WCSim"
-export G4WORKDIR="$data_dir/$name/WCSim"
 
 # set directory where log files will be saved
 export LOGDIR="/scratch/$USER/log/$name/"
@@ -24,6 +22,7 @@ mkdir -p "$LOGDIR"
 # cd to this directory so SLURM puts its logs there too
 cd "$LOGDIR"
 
+# submit jobs with desired options
 JOBTIME=`date` sbatch --time=0:59:00 --array=0-9 --job-name=e "$DATATOOLS/cedar_scripts/run_WCSim_job.sh" "$name" "$data_dir" -n 100 -e 100 -E 1000 -P e- -d 2pi -p fix -x 0 -y 0 -z 0
 JOBTIME=`date` sbatch --time=0:59:00 --array=0-9 --job-name=mu "$DATATOOLS/cedar_scripts/run_WCSim_job.sh" "$name" "$data_dir" -n 100 -e 100 -E 1000 -P mu- -d 2pi -p fix -x 0 -y 0 -z 0
 JOBTIME=`date` sbatch --time=0:59:00 --array=0-9 --job-name=gamma "$DATATOOLS/cedar_scripts/run_WCSim_job.sh" "$name" "$data_dir" -n 100 -e 100 -E 1000 -P gamma -d 2pi -p fix -x 0 -y 0 -z 0
